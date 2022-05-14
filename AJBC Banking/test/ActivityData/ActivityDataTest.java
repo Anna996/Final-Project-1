a58048d4978c944f26ef292746195e62ea6bab84
anna aba <annaaba15@gmail.com>
2022-05-14 15:09:23 +0300

    created test for ActivityData

---
+++ AJBC Banking/test/ActivityData/ActivityDataTest.java
@@ -0,0 +1,40 @@
+package ActivityData;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+class ActivityDataTest {
+
+	private ActivityData activity;
+	private LocalDateTime timeStamp;
+	private String info;
+	private double balanceChange;
+
+	public ActivityDataTest() {
+		timeStamp = LocalDateTime.now();
+		info = "info about deposit";
+		balanceChange = 20.5;
+		activity = new ActivityData(ActivityName.DEPOSIT, timeStamp, info, balanceChange);
+	}
+
+	@Test
+	void checkTimeStamp() {
+		assertEquals(timeStamp, activity.getTimeStamp());
+		assertNotEquals(LocalDateTime.now(), activity.getTimeStamp());
+	}
+	
+	@Test
+	void checkBalanceChange() {
+		assertEquals(balanceChange, activity.getBalanceChange());
+		assertNotEquals(20.4, activity.getBalanceChange());
+	}
+	
+	@Test
+	void checkInfo() {
+		assertEquals(info, activity.getInfo());
+		assertNotEquals(" ", activity.getInfo());
+	}
+}
